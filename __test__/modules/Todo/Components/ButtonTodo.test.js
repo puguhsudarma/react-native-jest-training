42cff6644460e1206c33c26d793fa41d07ff5492
Reroet <wayanpuguhsudarma@gmail.com>
2019-02-13 22:44:05 +0800

    test with enzyme

--- __test__/modules/Todo/Components/ButtonTodo.test.js
+++ __test__/modules/Todo/Components/ButtonTodo.test.js
@@ -1,18 +1,21 @@
 import React from "react";
-import renderer from "react-test-renderer";
-import ShallowRenderer from "react-test-renderer/shallow";
+import renderer from 'react-test-renderer';
+import ShallowRenderer from 'react-test-renderer/shallow';
 import ButtonTodo from "../../../../src/modules/Todo/Components/ButtonTodo";
 
 describe("Test Button Todo Render", () => {
+  // mock function
+  const actionFn = jest.fn();
+
   it("Render deep button todo correctly", () => {
-    const tree = renderer.create(<ButtonTodo action={() => {}} />);
+    const tree = renderer.create(<ButtonTodo action={actionFn} />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("Render shallow button todo correctly", () => {
-    const rendererInstance = new ShallowRenderer();
-    rendererInstance.render(<ButtonTodo action={() => {}} />);
-    const tree = rendererInstance.getRenderOutput();
+    const shallow = new ShallowRenderer();
+    shallow.render(<ButtonTodo action={actionFn} />);
+    const tree = shallow.getRenderOutput();
     expect(tree).toMatchSnapshot();
   });
 });
